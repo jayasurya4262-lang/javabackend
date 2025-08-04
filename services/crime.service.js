2c1c5a7ff4e5f2cc82e6d16ca39bb7c748c9dd7c
jayasurya4262-lang <jayasurya4262@gmail.com>
2025-08-04 21:06:51 +0530

    Add files via upload

---
+++ services/crime.service.js
@@ -0,0 +1,47 @@
+const Crime = require("../models/crime.model")
+
+const crimeService = {
+  saveCrime: async (crimeData) => {
+    if (crimeData.id) {
+      // If ID exists, it's an update operation
+      return Crime.findByIdAndUpdate(crimeData.id, crimeData, { new: true })
+    } else {
+      // Otherwise, it's a new crime
+      const newCrime = new Crime(crimeData)
+      return newCrime.save()
+    }
+  },
+
+  getAllCrimes: async () => {
+    return Crime.find()
+  },
+
+  getCrimeById: async (id) => {
+    return Crime.findById(id)
+  },
+
+  getCrimesByType: async (crimeType) => {
+    return Crime.find({ crimeType: { $regex: crimeType, $options: "i" } })
+  },
+
+  getCrimesByLocation: async (location) => {
+    return Crime.find({ location: { $regex: location, $options: "i" } })
+  },
+
+  searchCrimes: async (query) => {
+    const crimesByType = await Crime.find({ crimeType: { $regex: query, $options: "i" } })
+    const crimesByLocation = await Crime.find({ location: { $regex: query, $options: "i" } })
+    const crimesByDescription = await Crime.find({ description: { $regex: query, $options: "i" } })
+
+    // Combine and remove duplicates
+    const combinedResults = [...crimesByType, ...crimesByLocation, ...crimesByDescription]
+    const uniqueResults = Array.from(new Map(combinedResults.map((item) => [item["_id"].toString(), item])).values())
+    return uniqueResults
+  },
+
+  deleteCrime: async (id) => {
+    return Crime.findByIdAndDelete(id)
+  },
+}
+
+module.exports = crimeService
---
+++ services/user.service.js
@@ -0,0 +1,35 @@
+const User = require("../models/user.model")
+
+const userService = {
+  authenticateUser: async (username, password) => {
+    const response = {}
+    const user = await User.findOne({ username })
+
+    if (user && user.password === password) {
+      response.success = true
+      response.user = user
+      response.role = user.role
+    } else {
+      response.success = false
+      response.message = "Invalid credentials"
+    }
+    return response
+  },
+
+  registerUser: async (userData) => {
+    const existingUserByUsername = await User.exists({ username: userData.username })
+    const existingUserByEmail = await User.exists({ email: userData.email })
+
+    if (!existingUserByUsername && !existingUserByEmail) {
+      const newUser = new User(userData)
+      return newUser.save()
+    }
+    return null
+  },
+
+  getUserById: async (id) => {
+    return User.findById(id)
+  },
+}
+
+module.exports = userService
